1d32898238b8c9584382c294031ed9a85129a64e
yego <yegokipkemoi254@gmail.com>
2022-10-01 02:37:59 +0300

    Done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+  const getMovies = movies.map((movie)=>(
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <h3>{movie.time}</h3>
+      <ul>
+        {movie.genres.map((genre)=>(
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return (<div>
+    <h1>Movies Page</h1>
+    {getMovies}
+  </div>
+  )
 }
 
 export default Movies;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 
 ReactDOM.render(
+  <BrowserRouter>
   <Router>
     <App />
-  </Router>,
+  </Router>
+  </BrowserRouter>,
   document.getElementById("root")
 );
